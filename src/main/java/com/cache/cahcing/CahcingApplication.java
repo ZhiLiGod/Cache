2225af30c93774b7008c494b20b22f43871d5958
Zhi Li <zhi.li@invoicefair.com>
2020-08-05 22:23:18 +0100

    [feat] - first commit

---
+++ src/main/java/com/cache/cahcing/CahcingApplication.java
@@ -0,0 +1,13 @@
+package com.cache.cahcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CahcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CahcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cache/cahcing/CahcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cache.cahcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CahcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
